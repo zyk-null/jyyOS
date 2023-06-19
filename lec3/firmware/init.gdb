45e16945d08a4fc6508506e3a15010b72c46fb95
zyk-null <z688006@outlook.com>
2023-06-19 12:38:35 +0800

    add lec4

--- lec3/firmware/init.gdb
+++ lec3/firmware/init.gdb
@@ -6,6 +6,7 @@ end
 # Connect to remote
 target remote localhost:1234
 file a.out
+wa *0x7c00
 break *0x7c00
 layout src
 continue
---
+++ lec3/hello-os/build/x86_64-qemu/hello.d
@@ -0,0 +1,5 @@
+/home/xx/Documents/jyyOS/lec3/hello-os/build/x86_64-qemu/hello.o: \
+ /home/xx/Documents/jyyOS/lec3/hello-os/hello.c \
+ /home/xx/Documents/ics2022/abstract-machine/am/include/am.h \
+ /home/xx/Documents/ics2022/abstract-machine/am/include/arch/x86_64-qemu.h \
+ /home/xx/Documents/ics2022/abstract-machine/am/include/amdev.h
